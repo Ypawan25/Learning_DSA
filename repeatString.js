a1d50b7048ee8ad7a4809733f95804f861eead3a
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-02-27 16:08:15 +0530

    repeateString

---
+++ repeatString.js
@@ -0,0 +1,15 @@
+function repeatStringNumTimes(str, num) {
+  let str1 =""
+  if(num<=0)
+     return ""
+     else{
+       for(let i=0;i<num;i++){
+         str1 =str1 + str
+         
+       }
+     }
+    
+  return str1;
+}
+
+repeatStringNumTimes("abc", 3);
